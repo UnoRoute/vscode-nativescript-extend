81147bc3e236dcf5754ed57a321ddb4e8feec476
Paul Ehigie <ehigiepaul@yahoo.com>
2018-10-27 02:32:03 +0100

    deleted the old emulator

--- src/emulator/config.ts
+++
@@ -1,9 +0,0 @@
-import { workspace } from "vscode";
-
-const config = () => {
-  return workspace.getConfiguration("nativescript-extend");
-};
-
-export var emulatorPath = () => {
-  return config().get("emulatorPath");
-};
--- src/emulator/utils/message.ts
+++
@@ -1,9 +0,0 @@
-const { window } = require("vscode");
-
-export var showSuccessMessage = () => {
-  window.showInformationMessage("Emulator is booting up ...");
-};
-
-export var showErrorMessage = () => {
-  window.showErrorMessage("Emulator failed to boot.");
-};
