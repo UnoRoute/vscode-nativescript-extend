808e8bc7a4fb67e990a334ade1c8c8c407421b3e
ehigiepaul <ehigiepaul@yahoo.com>
2020-04-11 14:15:16 +0100

    fixed minilist

--- src/extension.ts
+++ src/extension.ts
@@ -7,37 +7,35 @@ import { snippet } from "./snippets/snippets";
 import { readFile } from "fs";
 import { join } from "path";
 import { tracking } from "./tracking/tracking";
-import { lsp } from './lsp/lsp';
+import { lsp } from "./lsp/lsp";
 
 export function activate(context: vscode.ExtensionContext) {
+  console.log(vscode.workspace.workspaceFolders[0].uri.fsPath);
+  var file = join(
+    vscode.workspace.workspaceFolders[0].uri.fsPath,
+    "package.json"
+  );
 
+  readFile(file, (err, data) => {
+    if (err) console.log("This is not a nativescript project");
 
-   console.log(vscode.workspace.workspaceFolders[0].uri.fsPath)
-  var file = join(vscode.workspace.workspaceFolders[0].uri.fsPath, "package.json")
-
-
-  readFile(file,  (err, data) => {
-    if (err) console.log('This is not a nativescript project')
-
-    var pack = JSON.parse(data.toString())
+    var pack = JSON.parse(data.toString());
     if (pack.nativescript) {
-      console.log(
-        'Congratulations, "Nativescript-Extend" is now active!'
-      );
+      console.log('Congratulations, "Nativescript-Extend" is now active!');
       //  enable extension if nativescript is found in the package.json
-      lsp(context)
+      lsp(context);
       new contextMenu(context);
       new Emulator(context);
       new snippet(context);
       new xsd(context);
-      new tracking(context)
+      new tracking(context);
     } else {
-      console.log('This is not a nativescript project')
+      console.log("This is not a nativescript project");
     }
-  })
+  });
 
   // new xml(context)
   // new validation(context)
 }
 
-export function deactivate() { }
+export function deactivate() {}
